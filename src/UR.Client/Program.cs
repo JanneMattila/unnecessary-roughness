f28745ddc3320122a86b41971aa5542fc0161ad6
Janne Mattila <janne.mattila@hotmail.fi>
2020-09-04 22:59:07 +0300

    Fix sample to match preview 8

--- src/UR.Client/Program.cs
+++ src/UR.Client/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -15,7 +15,7 @@ namespace UR.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 

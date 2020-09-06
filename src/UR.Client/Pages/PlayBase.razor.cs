097ed7478a7cb8682d6b166e2d129e6d2f28b9c0
Janne Mattila <janne.mattila@hotmail.fi>
2020-09-06 20:08:30 +0300

    Migrate old code

--- src/UR.Client/Pages/PlayBase.razor.cs
+++ src/UR.Client/Pages/PlayBase.razor.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using UR.Data;
 
 namespace UR.Client.Pages
 {
@@ -19,7 +17,65 @@ namespace UR.Client.Pages
         [Inject] private HttpClient Http { get; set; }
         [Inject] private IJSRuntime JSRuntime { get; set; }
 
+        protected static GameEngine _gameEngine;
+        protected static Action _stateHasChanged;
+        protected static IJSRuntime JSRuntime2;
         protected static ElementReference _canvas;
+
+        protected override async Task OnInitializedAsync()
+        {
+            // TODO: Fix static identifiers.
+            JSRuntime2 = JSRuntime;
+
+            _stateHasChanged = () => StateHasChanged();
+
+            await base.OnInitializedAsync();
+
+            if (_gameEngine == null)
+            {
+                var randomizer = new RandomizerBackend
+                {
+                    FetchDicesAsync = async (id, type, count) => await Http.GetStringAsync($"api/Randomizer/Dices/{id}/{type}/{count}")
+                };
+
+                var eventStore = new EventStoreBackend
+                {
+                    HttpPutEventAsync = async (id, e) => await Http.PutAsync($"api/Events/{id}", new StringContent(e, System.Text.Encoding.Unicode, "application/xml")),
+                    HttpGetEventsAsync = async (id) => await Http.GetStringAsync($"api/Events/{id}")
+                };
+
+                _gameEngine = new GameEngine(new BrowserLogger(), new Data.Game()
+                {
+                    ID = "123",
+                    HomeTeam = new Team(),
+                    VisitorTeam = new Team()
+                }, randomizer, eventStore)
+                {
+                    ExecuteAnimations = (playerAnimations, ballAnimation) =>
+                    {
+                        JSRuntime.InvokeAsync<object>("animate", playerAnimations, ballAnimation);
+                    },
+
+                    ExecuteDraw = (game) =>
+                    {
+                        JSRuntime.InvokeAsync<object>("drawCanvas", game);
+                        _stateHasChanged();
+                    },
+
+                    ShowElement = (id, modal) =>
+                    {
+                        JSRuntime.InvokeAsync<object>("showElement", id, modal);
+                        _stateHasChanged();
+                    },
+
+                    HideElement = (id) =>
+                    {
+                        JSRuntime.InvokeAsync<object>("hideElement", id);
+                        _stateHasChanged();
+                    }
+                };
+            }
+        }
 
         protected virtual void Dispose(bool disposing)
         {
@@ -27,7 +83,8 @@ namespace UR.Client.Pages
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects)
+                    // Dispose managed state (managed objects)
+                    _gameEngine = null;
                 }
 
                 // TODO: free unmanaged resources (unmanaged objects) and override finalizer
@@ -35,13 +92,6 @@ namespace UR.Client.Pages
                 disposedValue = true;
             }
         }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~PlayBase()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
 
         void IDisposable.Dispose()
         {
---
+++ src/UR/GameEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UR.Animation;
+using UR.Data;
+
+namespace UR
+{
+    public class GameEngine
+    {
+        private readonly IBrowserLogger _logger;
+        private readonly IRandomizer _randomizer;
+        private readonly IEventStore _eventStore;
+        private readonly Player[] _boardPositions = new Player[GameBoard.BoardWidth * GameBoard.BoardHeight];
+
+        private GameState _gameState = GameState.Initialization;
+        private Game _game;
+
+        public Action<ObjectAnimation[], ObjectAnimation> ExecuteAnimations;
+        public Action<Game> ExecuteDraw;
+        public Action<string, bool> ShowElement;
+        public Action<string> HideElement;
+
+        public GameEngine(IBrowserLogger logger, Game game, IRandomizer randomizer, IEventStore eventStore)
+        {
+            _logger = logger;
+            _randomizer = randomizer;
+            _eventStore = eventStore;
+            _game = game;
+        }
+    }
+}
